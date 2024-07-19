4f626fa4bbbacb7c1b154150648d1be00c8e2137
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 17:39:15 +0700

    Add page.tsx🫸🌀✏️📗🐧🐳⬆

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+// Importing the login form component
+import Form from "@/components/login/form";
+
+// Defining the asynchronous function for login page initialization
+export default async function LoginPage() {
+    return (
+        // Rendering the login form component
+        <Form/>
+    );
+}
