90ce25d5a0b4f1b0b4a45ffa86241d5a22ade675
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 17:41:09 +0700

    Add member settings page.tsx🫸🌀✏️📗🐧🐳⬆

---
+++ src/app/member/settings/page.tsx
@@ -0,0 +1,7 @@
+// Defining the Member Settings Page Component
+export default function Settings() {
+    // Rendering the Member Settings Page JSX content
+    return (
+        <div>Settings Page</div>
+    )
+}
