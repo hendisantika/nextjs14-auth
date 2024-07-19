70e1b13c86b3b1674a113722d5d409ced7e33fbb
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 17:29:06 +0700

    Add signup page.tsx🫸🌀✏️📗🐧🐳⬆

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+// Importing the Sign-Up Form component
+import Form from "@/components/signup/form";
+
+export default function SignUp() {
+    return (
+        // Rendering the Sign-Up Form component
+        <Form/>
+    );
+}
