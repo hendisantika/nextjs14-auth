c5eb05db44a26e8be46f989f23a6ee8aeb4338ed
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 17:33:12 +0700

    Add generatePasswordHash🫸🌀✏️📗🐧🐳⬆

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -130,3 +130,13 @@ export const findUserByEmail = async (email: string) => {
         },
     });
 };
+
+// Function to generate a hashed password
+const generatePasswordHash = async (password: string) => {
+    // generates a random salt. A salt is a random value used in the hashing process to ensure
+    // that even if two users have the same password, their hashed passwords will be different.
+    // The 10 in the function call represents the cost factor, which determines how much
+    // computational work is needed to compute the hash.
+    const salt = await bcryptjs.genSalt(10);
+    return bcryptjs.hash(password, salt);
+};
